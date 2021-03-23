ebc14c9883deafa4b9f833cb8506219af151d2aa
Marcha Couyoute <mcouyoute1@pride.hofstra.edu>
2021-03-23 19:38:19 -0400

    Changed files

--- top50_one.py
+++ top50_one.py
@@ -1,25 +1,2 @@
 import mysql.connector
 from mysql.connector.errors import Error
-
-
-def create_connection(path):
-    connection = None
-    try:
-        connection = mysql.connect(path)
-        print('Connection to DB is successful')
-    except Error as e:
-        print(f"The error '{e}' occured")
-    return connection
-
-
-connection = create_connection("C:\\top50.mysql")
-
-
-def execute_query(connection, query):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        connection.commit()
-        print("Query executed successfully")
-    except Error as e:
-        print(f"The error '{e}' occured")
