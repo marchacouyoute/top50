43526bbf0d5c234c6c3306da376c1574a1be9998
Marcha Couyoute <mcouyoute1@pride.hofstra.edu>
2021-03-23 17:57:23 -0400

    Added DB connection

---
+++ top50_one.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from mysql.connector.errors import Error
+
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = mysql.connect(path)
+        print('Connection to DB is successful')
+    except Error as e:
+        print(f"The error '{e}' occured")
+    return connection
+
+
+connection = create_connection("C:\\top50.mysql")
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+  try:
+      cursor.except(query)
+      connection.commit()
+      print("Query executed successfully")
+  except Error as e:
+      print(f"The error '{e}' occured")    
